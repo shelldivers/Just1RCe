da996540eb91f81d039dc60cadda24f05ae032ea
iphuck22 <eldeshue@naver.com>
2025-01-01 02:16:01 +0900

    refactor(Client) : refactor method
    refactor GetReceivedMessage
    before:
    - use static char array, recv_buffer as a buffer
    - use strstr, move char*
    after:
    - use read_buffer_ as a buffer, after appending read_buffer_ with recv_buffer
    - use string::find, move ssize_t
    
    to resolve potential error, combine two buffer read_buffer_ and recv_buffer

--- srcs/client/client_get_messages.cc
+++ srcs/client/client_get_messages.cc
@@ -20,7 +20,7 @@ std::vector<std::string> Client::GetReceivedMessages() {
   char recv_buffer[JUST1RCE_SRCS_CLIENT_MSG_MAX + 1];
 
   memset(recv_buffer, '\0', JUST1RCE_SRCS_CLIENT_MSG_MAX + 1);
-  int recv_size =
+  ssize_t recv_size =
       recv(socket_.socket_fd(), recv_buffer, JUST1RCE_SRCS_CLIENT_MSG_MAX, 0);
   if (recv_size == -1) {
     /* TODO, re-entering receive later
@@ -31,31 +31,31 @@ if (errno == EAGAIN || errno == EWOULDBLOCK) {
     throw std::runtime_error(JUST1RCE_SRCS_CLIENT_RECV_ERROR);
   }
 
+  // get result
   std::vector<std::string> result;
-  char *start_pos = recv_buffer, *end_pos = NULL;
+  size_t start_pos = 0, end_pos = 0;
   if (recv_size > 0) {
-    // search CR-LF to identify whole message
-    while (end_pos =
-               std::strstr(start_pos, JUST1RCE_SRCS_CLIENT_MESSAGE_DELIM)) {
-      // get whole message from buffer
-      const int msg_size = end_pos - start_pos;
-      std::string msg(start_pos, msg_size);
+    // combine recv_buffer and left over read_buffer_
+    read_buffer_.append(recv_buffer, recv_size);
 
-      // combine with the previous left buffer
-      if (!read_buffer_.empty()) {
-        msg = read_buffer_ + msg;
-        read_buffer_.clear();
-      }
+    // split result with delimeter
+    while ((end_pos = read_buffer_.find(JUST1RCE_SRCS_CLIENT_MESSAGE_DELIM,
+                                        start_pos)) != std::string::npos) {
+      // get whole message from read_buffer
+      std::string msg = read_buffer_.substr(start_pos, (end_pos - start_pos));
+
       // push whole message
       result.push_back(msg);
 
       // move search position
-      recv_size -= msg_size + 2;  // size left in the buffer
-      start_pos = end_pos + 2;    // move after cr-lf
+      start_pos = end_pos + 2;  // move after cr-lf
     }
   }
   // save left over text
-  read_buffer_ = std::string(start_pos, recv_size);
+  // erase used messages
+  if (start_pos != 0)
+    read_buffer_ =
+        read_buffer_.substr(start_pos, read_buffer_.size() - start_pos);
 
   // return
   return result;
