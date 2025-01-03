a41df8ec9681d08c853d4bbbc549542350f1198d
kenWR <skyqkqk1168@gmail.com>
2025-01-03 16:24:05 +0900

    chore: remove all the unuse files
    - remove all the unuse .in files
    - remove all the unuse test files
    - remove all the unuse scripts

--- tests/test_gtest.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(Hello2Test, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 5, 42);
-}
