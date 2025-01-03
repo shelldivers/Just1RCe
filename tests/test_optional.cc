54c0ef686c6880c514e32054fad7f2a1224a3a07
kenWR <skyqkqk1168@gmail.com>
2025-01-03 21:24:58 +0900

    test: add test_optional.cc
    - this test_optional.cc is a test file for testing optional class
    - and this is show how to use google test framework
    - there is few elements to run google test
    	- include <gtest/gtest.h>
    	- use EXPECT_* macro for run all test
    	- need main function
    		- init google test by calling InitGoogleTest
    		  ::testing::InitGoogleTest(&argc, argv);
    		- run all test by calling RUN_ALL_TESTS
    		  return RUN_ALL_TESTS();
    		- if you want to run other options, you can find commands from here
    		  > https://google.github.io/googletest/

--- tests/test_optional.cc
+++ tests/test_optional.cc
@@ -14,10 +14,10 @@ TEST(FTOptionalTest, DefaultInitialization) {
     ft::optional<std::string> test1;
     EXPECT_FALSE(test1.has_value());
 
-    test1 = std::string("hello world");
-    EXPECT_TRUE(test1.has_value());
-    EXPECT_EQ(test1.value(), "hello world");
-    EXPECT_EQ(*test1, "hello world");
+    ft::optional<std::string> test2("hello world");
+    EXPECT_TRUE(test2.has_value());
+    EXPECT_EQ(test2.value(), "hello world");
+    EXPECT_EQ(*test2, "hello world");
 }
 
 TEST(FTOptionalTest, ConstReferenceAccess) {
@@ -49,61 +49,12 @@ TEST(FTOptionalTest, VectorStringHandling) {
     (*test3).push_back("hello");
 
     EXPECT_TRUE(test3.has_value());
-    ASSERT_EQ(test3->size(), 1);
     EXPECT_EQ((*test3)[0], "hello");
 }
 
-
-// #include <iostream>
-// #include <string>
-// #include <vector>
-
-// #include "../includes/ft_optional.h"
-
-// int main() {
-//   ft::optional<std::string> test("test construction");
-//   std::cout << "Dose test has value ? : " << std::boolalpha << test.has_value()
-//             << '\n';
-//   std::cout << "the value is : " << test.value() << '\n';
-
-//   std::cout << "Before initializing test1.\n";
-//   ft::optional<std::string> test1;
-//   std::cout << "Dose test1 has value ? : " << std::boolalpha
-//             << test1.has_value() << '\n';
-
-//   std::cout << "After initializing.\n";
-//   test1 = std::string("hello world");
-//   std::cout << "Dose test1 has value ? : " << std::boolalpha
-//             << test1.has_value() << '\n';
-//   std::cout << "the value is : " << test1.value() << '\n';
-//   std::cout << "the value is : " << *test1 << '\n';
-
-//   std::cout << "Const ref of test1\n";
-//   const ft::optional<std::string> &ref_test1 = test1;
-//   std::cout << "the value is : " << ref_test1.value() << '\n';
-//   std::cout << "the value is : " << *ref_test1 << '\n';
-
-//   std::cout << "ref of test1\n";
-//   std::cout << "the value is : " << ref_test1.value() << '\n';
-//   std::cout << "the value is : " << *ref_test1 << '\n';
-
-//   std::cout << "mutability test\n";
-//   *test1 = "goodbye world";
-//   std::cout << "the value is : " << test1.value() << '\n';
-//   std::cout << "the value is : " << *test1 << '\n';
-
-//   // how about primal type, no destructor
-//   std::cout << "initializing test2.\n";
-//   ft::optional<int> test2(0);
-//   std::cout << "Dose test2 has value ? : " << std::boolalpha
-//             << test1.has_value() << '\n';
-//   std::cout << "the value is : " << test2.value() << '\n';
-
-//   // optional vector string
-//   std::cout << "optional + vector + string tests\n";
-//   ft::optional<std::vector<std::string> > test3((std::vector<std::string>()));
-//   (*test3).push_back("hello\n");
-//   std::cout << (*test3)[0];
-// }
-
+// Google Test 실행을 위한 메인 함수
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);  // Google Test 초기화
+    return RUN_ALL_TESTS();                 // 모든 테스트 실행
+}
 
