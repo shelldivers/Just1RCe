d61edd0577eacab19fb09a3c766f693211df3522
kenWR <skyqkqk1168@gmail.com>
2025-01-04 17:14:10 +0900

    chore: add test for fail cppcheck cpplint

--- main.cc
+++ main.cc
@@ -1,4 +1,12 @@
-int main(void)
-{
-	return 0;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 42, y = 0;
+    int result = x / y;  // Intentional division by zero to trigger cppcheck warning
+
+    cout<<"Result is: "<<result<<endl;  // No space around operators (cpplint issue)
+
+    int* ptr = new int(5);
+    return 0;  // Memory leak: `ptr` is not deleted (cppcheck warning)
 }
