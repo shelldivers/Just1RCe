aa359a74facfae9312e4dda5e92c4b28f94cbe58
iphuck22 <eldeshue@naver.com>
2025-01-04 17:06:14 +0900

    feat(Channel) : implement constructor, modify method
    Channel 클래스에 대한 생성자를 정의함
    - 멤버 변수 초기화
    - default 모드 세팅
    - key값 유무에 따른 모드 세팅
    - 잘못된 체널 이름에 따른 error throw
    - 체널을 생성한 사용자를 현제 체널의 user로 등록
    user_num getter 메서드 변경
    - user_num_ 멤버 변수를 삭제하고, 등록된 user의 갯수를 반환
    
    user_nicknames_에 등록된 사용자 이름의 수가 곧 사용자의 수, 따라서 user_num_은 불필요한 데이터라 삭제함

--- srcs/channel/channel.cc
+++ srcs/channel/channel.cc
@@ -1,14 +1,29 @@
 
 #include "../includes/channel.h"
 
+#include <limits>
 #include <stdexcept>
 #include <string>
 
 namespace Just1RCe {
 
 Channel::Channel(std::string const &name, std::string const &oprt_name,
-                 std::string const key = "") {};
+                 std::string const key = "")
+    : name_(name),
+      topic_(""),
+      key_(key),
+      mod_(JUST1RCE_SRCS_CHANNEL_MOD_DEFAULT),
+      max_user_num_(std::numeric_limits<size_t>::max()) {
+  // wrong channel name format error
+  if (!IS_CHANNEL_GLOBAL && !IS_CHANNEL_LOCAL && !IS_CHANNEL_MOELESS &&
+      !IS_CHANNEL_SAFE)
+    throw std::runtime_error(JUST1RCE_SRCS_CHANNEL_WRONG_NAME_ERROR);
+  user_nicknames_.insert(oprt_name);
 
+  if (!key_.empty()) {
+    mod_ |= JUST1RCE_SRCS_CHANNEL_MOD_KEY_SET;
+  }
+}
 
 Channel::~Channel() {}
 
@@ -17,7 +32,7 @@ std::string Channel::name() const { return name_; }
 std::string Channel::topic() const { return topic_; }
 std::string Channel::key() const { return key_; }
 size_t Channel::max_user_num() const { return max_user_num_; }
-size_t Channel::user_num() const { return user_num_; }
+size_t Channel::user_num() const { return user_nicknames_.size(); }
 
 // setter
 void Channel::set_topic(std::string const &new_topic) { topic_ = new_topic; }
