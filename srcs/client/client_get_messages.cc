307c42b8cf98b54209a13014ccb0bd1111bf0b9a
iphuck22 <eldeshue@naver.com>
2025-01-13 19:59:02 +0900

    refactor(Client) : split GetReceivedMessages \#38

--- srcs/client/client_get_messages.cc
+++ srcs/client/client_get_messages.cc
@@ -14,59 +14,63 @@ extern "C" {
 namespace Just1RCe {
 
 /**
+ * @brief recv text from socket's fd and append text to the buffer
+ * @param fd fd of the socket
+ * @param buffer buffer to save received text
+ */
+static bool recv_and_append_buffer(int const fd, std::string &buffer) {
+  char recv_buffer[JUST1RCE_SRCS_CLIENT_MSG_MAX + 1];
+  ssize_t recv_size = 0;
+
+  memset(recv_buffer, '\0', JUST1RCE_SRCS_CLIENT_MSG_MAX + 1);
+  while (recv_size = recv(fd, recv_buffer, JUST1RCE_SRCS_CLIENT_MSG_MAX, 0)) {
+    // recv failed
+    if (recv_size == -1) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        return false;  // empty option,
+                       // equivalent to
+                       // std::noops
+      }
+      throw std::runtime_error(JUST1RCE_SRCS_CLIENT_RECV_ERROR);
+    }
+    // recv succed, append buffer with the received text
+    buffer.append(recv_buffer, recv_size);
+  }
+  return true;
+}
+
+/**
+ * @brief split result with CR-LF
+ */
+static std::vector<std::string> split_string_with_crlf(std::string &str) {
+  std::vector<std::string> buffered_messages;
+  size_t start_pos = 0, end_pos = 0;
+
+  while ((end_pos = str.find(JUST1RCE_SRCS_CLIENT_MESSAGE_DELIM, start_pos)) !=
+         std::string::npos) {
+    // get whole message from str
+    buffered_messages.push_back(str.substr(start_pos, (end_pos - start_pos)));
+
+    // move start position to search next delimeter delimeter
+    start_pos = end_pos + 2;  // move after cr-lf
+  }
+  // save left over text
+  // erase used messages
+  if (start_pos != 0) str = str.substr(start_pos, str.size() - start_pos);
+  return buffered_messages;
+}
+
+/**
  * @brief receive raw text from socket and turn it to propeer messages
  * @return vector of strings
  * @throws runtime error, recv failure
  */
 ft::optional<std::vector<std::string> > Client::GetReceivedMessages() {
-  char recv_buffer[JUST1RCE_SRCS_CLIENT_MSG_MAX + 1];
-  memset(recv_buffer, '\0', JUST1RCE_SRCS_CLIENT_MSG_MAX + 1);
+  if (!recv_and_append_buffer(this->socket_.socket_fd(), this->read_buffer_))
+    return ft::optional<std::vector<std::string> >();
 
-  // recv text data from socket till the socket's buffer is empty
-  ssize_t total_recv_size = 0, recv_size = 0;
-  while (recv_size = recv(socket_.socket_fd(), recv_buffer,
-                          JUST1RCE_SRCS_CLIENT_MSG_MAX, 0)) {
-    // recv failed
-    if (recv_size == -1) {
-      if (errno == EAGAIN || errno == EWOULDBLOCK) {
-        return ft::optional<std::vector<std::string> >();  // empty option,
-                                                           // equivalent to
-                                                           // std::noops
-      }
-      throw std::runtime_error(JUST1RCE_SRCS_CLIENT_RECV_ERROR);
-    }
-    // recv succed, append read_buffer_ with the received text
-    read_buffer_.append(recv_buffer, recv_size);
-    total_recv_size += recv_size;
-  }
-
-  // get result
-  ft::optional<std::vector<std::string> > buffered_messages;
-  size_t start_pos = 0, end_pos = 0;
-  if (total_recv_size > 0) {
-    // split result with delimeter
-    while ((end_pos = read_buffer_.find(JUST1RCE_SRCS_CLIENT_MESSAGE_DELIM,
-                                        start_pos)) != std::string::npos) {
-      // result is option, use after initialization
-      if (!buffered_messages.has_value()) {
-        *buffered_messages = std::vector<std::string>();
-      }
-
-      // get whole message from read_buffer and push
-      (*buffered_messages)
-          .push_back(read_buffer_.substr(start_pos, (end_pos - start_pos)));
-
-      // move search position
-      start_pos = end_pos + 2;  // move after cr-lf
-    }
-  }
-  // save left over text
-  // erase used messages
-  if (start_pos != 0)
-    read_buffer_ =
-        read_buffer_.substr(start_pos, read_buffer_.size() - start_pos);
-
-  return buffered_messages;
+  return ft::optional<std::vector<std::string> >(
+      split_string_with_crlf(this->read_buffer_));
 }
 
 }  // namespace Just1RCe
