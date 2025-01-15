356b6d4b66824cf167fb6ac3a1cb02d6e9d1f462
Dohoon Gwak <96526280+eldeshue@users.noreply.github.com>
2025-01-15 15:55:45 +0900

    19 feat channel object (#41)
    * feat(Channel) : add declaration of Channel class
    
    declare definition of channel class
    fields:
    - info of the channel
    - info of users belongs to the channel
    - mode of the channel
    methods:
    - constructor / destructor
    - getter / setter
    - handling modes
    - handling users
    
    this class will be handled by std::map<std::string, Channel *> channel_manager
    
    * feat(Channel) : implement getter and setter
    
    * feat(Channel) : add definitions
    
    다음의 내용을 channel 헤더에 추가함
    - 에러 메시지
    - 체널의 모드를 표현하는 플래그(bit mask)
    - 체널 이름의 접두어를 바탕으로 체널 타입 체크하는 매크로
    이 외에도 일부 불필요한 멤버 변수를 삭제함
    
    * feat(Channel) : implement constructor, modify method
    
    Channel 클래스에 대한 생성자를 정의함
    - 멤버 변수 초기화
    - default 모드 세팅
    - key값 유무에 따른 모드 세팅
    - 잘못된 체널 이름에 따른 error throw
    - 체널을 생성한 사용자를 현제 체널의 user로 등록
    user_num getter 메서드 변경
    - user_num_ 멤버 변수를 삭제하고, 등록된 user의 갯수를 반환
    
    user_nicknames_에 등록된 사용자 이름의 수가 곧 사용자의 수, 따라서 user_num_은 불필요한 데이터라 삭제함
    
    * chore(Channel) : add comment to the constructor
    
    * feat(Channel) : implement mothods
    
    implemtent methods handles mod
    - GetModeAsString
    - GetMode
    - SetMode
    - UnsetMode
    - CheckMode
    
    * feat(Channel) : implement methods
    
    implement mothods that handles user's nickname
    - AddUser : 해당 채널에 소속되는 유저 추가
    - DeleteUser : 해당 채널에 소속된 유저 제거
    - CheckUser : 해당 사용자의 채널 소속 여부
    
    * style(Channel) : minor fix
    
    * fix(Channel) : minor fix
    
    * fix(Channel) : fix syntax error
    
    * feat(Channel) : erase codes related to the user's name
    
    사용자(user_nicknames_) 관련 코드를 모두 제거
    
    DBContext 도입으로 인해서 더 이상 Channel에서 사용자 관련 데이터를
    다루지 않게 되었음.
    
    * feat(Channel) : disable implicit call to Channel constructor
    
    before:
    Channel(...);
    after:
    explicit Channel(...);
    
    암시적 형변환을 막기 위해서 explicit 키워드를 Channel의 생성자에 추가
    
    * feat(Channel) : add inline functions that check channel's type \#19
    
    채널의 타입을 검사하는 함수를 변경
    기존의 매크로 함수를 삭제하고 동일한 역할을 수행하는 인라인 함수를 추가
    modeless channel과 safe channel관련 부분은 삭제하였음
    
    * \#19 fix(Channel) : check local channel name \#41
    
    -before
    if (!is_channel_global() && !is_channel_global())
    -after
    if (!is_channel_global() && !is_channel_local())
    
    채널 이름의 유효성 검증 과정에서 local channel 이름을 확인하는 부분을 추가

---
+++ srcs/channel/channel.cc
@@ -0,0 +1,62 @@
+
+#include "../includes/channel.h"
+
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+namespace Just1RCe {
+
+/**
+ * @brief constructor of the channel object.
+ * @param name name of the channel. First character of the name indicates the
+ * type of channel. Empty name must be considered as parsing error.
+ * @param oprt_name name of the first user who JOIN the channel. Client wth this
+ * name must be masked as operator.
+ * @param key password for the channel. if pw is not setted, key must be impty.
+ * @throw if 'name' is not formatted properly, throw runtime_exception
+ */
+explicit Channel::Channel(std::string const &name, std::string const &key = "")
+    : name_(name),
+      topic_(""),
+      key_(key),
+      mod_(JUST1RCE_SRCS_CHANNEL_MOD_DEFAULT),
+      max_user_num_(std::numeric_limits<size_t>::max()) {
+  // wrong channel name format error
+  if (!is_channel_global() && !is_channel_local())
+    throw std::runtime_error(JUST1RCE_SRCS_CHANNEL_WRONG_NAME_ERROR);
+
+  if (!key_.empty()) {
+    mod_ |= JUST1RCE_SRCS_CHANNEL_MOD_KEY_SET;
+  }
+}
+
+Channel::~Channel() {}
+
+// getter
+std::string Channel::name() const { return name_; }
+std::string Channel::topic() const { return topic_; }
+std::string Channel::key() const { return key_; }
+size_t Channel::max_user_num() const { return max_user_num_; }
+
+// setter
+void Channel::set_topic(std::string const &new_topic) { topic_ = new_topic; }
+void Channel::set_key(std::string const &new_key) { key_ = new_key; }
+size_t Channel::set_max_user_num(size_t const new_max_user_num) {
+  max_user_num_ = new_max_user_num;
+}
+
+/**
+ * @brief check channel name's prefix and return true if it is global
+ */
+inline bool Channel::is_channel_global() const {
+  return (this->name_[0] == '#');
+}
+
+/**
+ * @brief check channel name's prefix and return true if it is local
+ */
+inline bool Channel::is_channel_local() const {
+  return (this->name_[0] == '&');
+}
+}  // namespace Just1RCe
---
+++ srcs/channel/channel_mode.cc
@@ -0,0 +1,38 @@
+
+#include <stdexcept>
+#include <string>
+
+#include "../includes/channel.h"
+
+namespace Just1RCe {
+
+ChannelModeMask Channel::GetMode() const { return mod_; }
+
+/**
+ * @brief get mode of the channel in formatted string. This method is for MODE
+ * @return formatted string of channel's mode. ex) "+intk"
+ */
+std::string Channel::GetModeAsString() const {
+  if (mod_ == JUST1RCE_SRCS_CHANNEL_MOD_NONE) return "";
+  std::string result("+");
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_INVITE_ONLY) result.push_back('i');
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_MSG_OPRT_ONLY) result.push_back('m');
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_MSG_IN_ONLY) result.push_back('n');
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_TOPIC_OPRT_ONLY) result.push_back('t');
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_KEY_SET) result.push_back('k');
+  if (mod_ & JUST1RCE_SRCS_CHANNEL_MOD_USER_LIMIT) result.push_back('l');
+  return result;
+}
+
+void Channel::SetMode(ChannelModeMask const flags) { mod_ |= flags; }
+void Channel::UnsetMode(ChannelModeMask const flags) { mod_ &= ~flags; }
+
+/**
+ * @brief compare a mask
+ * @param flags mask to compare
+ * @return if flags and mod_ has common, return true.
+ */
+bool Channel::CheckMode(ChannelModeMask const flags) const {
+  return (mod_ & flags);
+}
+}  // namespace Just1RCe
