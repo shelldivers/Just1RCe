561e9cd6217443e65b6a1eb229f317a130ec2829
iphuck22 <eldeshue@naver.com>
2025-02-03 15:03:47 +0900

    refactor(client_mode) : change name of the function

--- srcs/client/client_mode.cc
+++ srcs/client/client_mode.cc
@@ -1,51 +1,59 @@
 
-#include <string>
+#include "../../includes/client_mode.h"
 
-#include "../../includes/client.h"
+#include <string>
 
 namespace Just1RCe {
 
 /**
  * @brief stringify current mode of the client
+ * @param target target mask to be converted
  * @return formatted mode of client
  * @throws none
  */
-std::string Client::GetModeAsString() const {
+std::string ConvertModeToString(ClientModeMask const target) {
   std::string result("+");
-  if (mode_mask_ & JUST1RCE_SRCS_CLIENT_MOD_INVISIBLE) result.push_back('i');
-  if (mode_mask_ & JUST1RCE_SRCS_CLIENT_MOD_WALLOPS) result.push_back('w');
-  if (mode_mask_ & JUST1RCE_SRCS_CLIENT_MOD_OPERATOR) result.push_back('o');
-  if (mode_mask_ & JUST1RCE_SRCS_CLIENT_MOD_REGISTERED) result.push_back('r');
-  if (mode_mask_ & JUST1RCE_SRCS_CLIENT_MOD_RECEIVE_SERVER)
-    result.push_back('s');
+  if (target & JUST1RCE_SRCS_CLIENT_MOD_INVISIBLE) result.push_back('i');
+  if (target & JUST1RCE_SRCS_CLIENT_MOD_WALLOPS) result.push_back('w');
+  if (target & JUST1RCE_SRCS_CLIENT_MOD_OPERATOR) result.push_back('o');
+  if (target & JUST1RCE_SRCS_CLIENT_MOD_REGISTERED) result.push_back('r');
+  if (target & JUST1RCE_SRCS_CLIENT_MOD_RECEIVE_SERVER) result.push_back('s');
 
   return result;
 }
 
 /**
  * @brief set bitset of flags to the client's mode
+ * @param target target mask to add flags
  * @param flags bitset of flags to set
  * @return none
  * @throws none
  */
-void Client::SetMode(uint const flags) { mode_mask_ |= flags; }
+inline void AddFlagsToMode(ClientModeMask &target, ClientModeMask const flags) {
+  target |= flags;
+}
 
 /**
  * @brief unset bitset of flags to the client's mode
+ * @param target target mask to sub flags
  * @param flags bitset of flags to unset
  * @return none
  * @throws none
  */
-void Client::UnsetMode(uint const flags) { mode_mask_ &= ~flags; }
+inline void SubFlagsFromMode(ClientModeMask &target,
+                             ClientModeMask const flags) {
+  target &= ~flags;
+}
 
 /**
  * @brief chekc currently setted mode of the client
+ * @param target mode to compare with
  * @param flags bitset of flags to compare
  * @return true if all the mask in the flags are setted.
  * @throws none
  */
-bool Client::CheckMode(uint const flags) const {
-  return (flags & mode_mask_) == flags;
+inline bool CheckMode(ClientModeMask const target, ClientModeMask const flags) {
+  return (flags & target) == flags;
 }
 
 }  // namespace Just1RCe
