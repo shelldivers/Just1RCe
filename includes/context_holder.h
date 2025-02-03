8175d2d3c054fe493e74030aca229116ffa63e4c
iphuck22 <eldeshue@naver.com>
2025-02-03 19:25:37 +0900

    feat : implement contextholder
    singleton, data manager

---
+++ includes/context_holder.h
@@ -0,0 +1,34 @@
+
+#ifndef CONTEXT_HOLDER_H
+#define CONTEXT_HOLDER_H
+
+namespace Just1RCe {
+
+class DbContext;
+
+/**
+ * @date 2025-02-03
+ * @brief class that handle DbContext, singleton pattern
+ * @author eldeshue
+ */
+class ContextHolder {
+ private:
+  ContextHolder();
+  ContextHolder(ContextHolder const &);
+  ~ContextHolder();
+
+  ContextHolder &operator=(ContextHolder const &);
+
+  ContextHolder(DbContext *ptr);
+  DbContext *const db_;
+
+  static ContextHolder *g_instance_ptr;
+
+ public:
+  static ContextHolder *GetInstance(DbContext *ptr);
+  DbContext *db();
+};
+
+}  // namespace Just1RCe
+
+#endif
---
+++ srcs/context_holder/context_holder.cc
@@ -0,0 +1,19 @@
+
+#include "../../includes/context_holder.h"
+
+#include <cstdlib>
+
+namespace Just1RCe {
+
+ContextHolder::ContextHolder(DbContext *ptr) : db_(ptr) {}
+
+ContextHolder::~ContextHolder() { delete db_; }
+
+ContextHolder *ContextHolder::GetInstance(DbContext *ptr) {
+  if (g_instance_ptr == NULL) g_instance_ptr = new ContextHolder(ptr);
+  return g_instance_ptr;
+}
+
+DbContext *ContextHolder::db() { return db_; }
+
+}  // namespace Just1RCe
