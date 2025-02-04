cab71ceba220b6440353322468cb957e3b478b45
정지원 <111475563+KenWR@users.noreply.github.com>
2025-02-04 03:37:07 +0000

    feat: 프린트 전용 함수 추가

--- srcs/main.cc
+++ srcs/main.cc
@@ -1,19 +1,23 @@
 #include "../includes/parser.h"
 #include <iostream>
 
+void printParsed(Just1RCe::Parser& parser) {
+    std::cout << "command :" << parser.command_ << std::endl << std::endl;
 
+    for (int i = 0; i < parser.middle_size_; ++i) {
+        std::cout << "middle :" << parser.middle_[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "trailing :" << parser.trailing_ << std::endl;
+}
 
 int main() {
     Just1RCe::Parser parser;
     parser.parse("JOIN #a,#b,#c 111,222,333");
+    printParsed(parser);
 
-    std::cout << "command: " << parser.command_ << std::endl;
-
-    for (int i = 0; i < parser.middle_size_; ++i) {
-        std::cout << "middle: " << parser.middle_[i] << std::endl;
-    }
-
-    std::cout << "trailing: ";
-    std::cout << parser.trailing_ << std::endl;
+    parser.parse("NICK :new_nickname");
+    printParsed(parser);
     return 0;
 }
