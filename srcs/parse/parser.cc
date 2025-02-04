0de7d7adf12e0e0e1dffcc5c1139167b71ac70df
kenWR <skyqkqk1168@gmail.com>
2025-02-04 11:37:48 +0900

    feat: 이름 변경 및 파서 구현 추가

---
+++ srcs/parse/parser.cc
@@ -0,0 +1,32 @@
+#include "../../includes/parser.h"
+#include <vector>
+#include <string>
+
+namespace Just1RCe {
+
+void Parser::parse(const std::string& message) {
+	if (message.empty()) return;
+
+	middle_size_ = 0;
+	is_trailing_ = false;
+	int before_index = 0;
+
+	for (int current_index = 0; current_index < message.size(); ++current_index) {
+		if (message[current_index] == JUST1RCE_SPACE) {
+			if (middle_size_) {
+				middle_[middle_size_++] = message.substr(before_index + 1, current_index);
+				before_index = current_index;
+			} else {
+				command_ = message.substr(0, current_index);
+				before_index = current_index;
+			}
+		}
+		else if (message[current_index] == JUST1RCE_COLON) {
+			is_trailing_ = true;
+			trailing_ = message.substr(current_index + 1, message.size() - current_index);
+			break;
+		}
+	}
+}	
+
+}
