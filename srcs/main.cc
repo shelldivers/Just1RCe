31582f697d605e2eec24efa627b34f1ddf4ef4b7
kenWR <skyqkqk1168@gmail.com>
2025-02-04 11:20:23 +0900

    feat: 테스트를 위한 main 문 수정정

--- srcs/main.cc
+++ srcs/main.cc
@@ -1,3 +1,19 @@
+#include "../includes/parser.h"
+#include <iostream>
+
+
+
 int main() {
+    Just1RCe::Parser parser;
+    parser.parse("JOIN #a,#b,#c 111,222,333");
+
+    std::cout << "command: " << parser.command_ << std::endl;
+
+    for (int i = 0; i < parser.middle_size_; ++i) {
+        std::cout << "middle: " << parser.middle_[i] << std::endl;
+    }
+
+    std::cout << "trailing: ";
+    std::cout << parser.trailing_ << std::endl;
     return 0;
 }
