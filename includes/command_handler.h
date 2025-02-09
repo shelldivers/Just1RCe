4a6671f68d1481e9d602d0c600c3042e95b598da
iphuck22 <eldeshue@naver.com>
2025-02-09 17:10:30 +0900

    feat(CommandHandler) : define interface of command's handler
    메세지와 해당 메시지를 보낸 client의 nickname를 인자로 받아서
    요청을 처리하는 함수 객체의 인터페이스를 정의한다
    커맨드 매핑에 의해서 호출되고, 관리될 예정이다.

---
+++ includes/command_handler.h
@@ -0,0 +1,26 @@
+
+#ifndef JUST1RCE_SRCS_COMMAND_HANDLER_H
+#define JUST1RCE_SRCS_COMMAND_HANDLER_H
+
+#include <string>
+
+namespace Just1RCe {
+
+/**
+ *
+ * @brief interface for command handler, will be mappend with command string
+ * @date 2025-02-09
+ * @author eldeshue
+ *
+ */
+class CommandHandler {
+ public:
+  ~CommandHandler() {}
+
+  virtual void operator()(std::string const &command,
+                          std::string const &meesage) = 0;
+};
+
+}  // namespace Just1RCe
+
+#endif
