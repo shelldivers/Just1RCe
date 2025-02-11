79a5ddf36b3456074690a1702b52fb0e5d38d27b
iphuck22 <eldeshue@naver.com>
2025-02-11 12:50:50 +0900

    feat : modify CommadHandler interface
    변경점
    - param : std::string const nick_name -> int const cleint_fd
    - return value : void -> std::vector<int>
    
    DbContext의 client에 대한 primary key가 fd로 변경됨에 따라, 인자를 변경
    이벤트 모듈과의 의존성 분리를 위해서 command의 수행 결과
    event의 등록이 필요한 fd를 vector에 모아서 return함.

--- includes/command_handler.h
+++ includes/command_handler.h
@@ -3,6 +3,7 @@
 #define JUST1RCE_SRCS_COMMAND_HANDLER_H
 
 #include <string>
+#include <vector>
 
 namespace Just1RCe {
 
@@ -17,8 +18,15 @@ class CommandHandler {
  public:
   ~CommandHandler() {}
 
-  virtual void operator()(std::string const &nick_name,
-                          std::string const &meesage) = 0;
+  /**
+   * @brief handle command
+   * @param client_fd fd to identify client that te message originate from
+   * @param message raw text message from client, will be parsed, no trailing
+   * cr-lf
+   * @return vector of int(fd), to register write event
+   */
+  virtual std::vector<int> operator()(int const client_fd,
+                                      std::string const &meesage) = 0;
 };
 
 }  // namespace Just1RCe
