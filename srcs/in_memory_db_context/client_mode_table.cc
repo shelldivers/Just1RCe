a73c8bedc76b18feaeea7e7e3053e7bd4766c5ee
iphuck22 <eldeshue@naver.com>
2025-02-12 15:02:16 +0900

    feat : modify implementation based on DbContext
    DbContext의 변화(fd를 primary key로 변경)에 맞춰서 구현 변경

--- srcs/in_memory_db_context/client_mode_table.cc
+++ srcs/in_memory_db_context/client_mode_table.cc
@@ -6,23 +6,23 @@
 
 namespace Just1RCe {
 
-void InMemoryDbContext::SetClientMode(std::string const &client_nick_name,
+void InMemoryDbContext::SetClientMode(int const client_fd,
                                       std::string const &channel_name,
                                       ClientModeMask mask) {
-  ModeKey key = std::make_pair(client_nick_name, channel_name);
+  ModeKey key = std::make_pair(client_fd, channel_name);
   mode_table_[key] = mask;
   return;
 }
 
 ClientModeMask InMemoryDbContext::GetClientMode(
-    std::string const &client_nick_name, std::string const &channel_name) {
-  ModeKey key = std::make_pair(client_nick_name, channel_name);
+    int const client_fd, std::string const &channel_name) {
+  ModeKey key = std::make_pair(client_fd, channel_name);
   return mode_table_[key];
 }
 
-void InMemoryDbContext::DeleteClientMode(std::string const &client_nick_name,
+void InMemoryDbContext::DeleteClientMode(int const client_fd,
                                          std::string const &channel_name) {
-  ModeKey key = std::make_pair(client_nick_name, channel_name);
+  ModeKey key = std::make_pair(client_fd, channel_name);
   ModeTableIter targetIter = mode_table_.find(key);
 
   // sanity check and erase
@@ -34,14 +34,13 @@ void InMemoryDbContext::DeleteClientMode(std::string const &client_nick_name,
  *
  * @brief delete all modes with same nick name. using range erase query
  */
-void InMemoryDbContext::DeleteClientModesByClientName(
-    std::string const &client_nick_name) {
+void InMemoryDbContext::DeleteClientModesByClientFd(int const client_fd) {
   // using lowerbound and upperbound, get range of iterator has 'nick_name'
   ModeTableIter left = mode_table_.lower_bound(
-      std::make_pair(client_nick_name, std::string(1, static_cast<char>(0))));
+      std::make_pair(client_fd, std::string(1, static_cast<char>(0))));
   ModeTableIter right = mode_table_.upper_bound(
-      std::make_pair(client_nick_name, std::string(1, static_cast<char>(255))));
-  mode_table_.erase(left, right);
+      std::make_pair(client_fd, std::string(1, static_cast<char>(255))));
+  for (ModeTableIter itr = left; itr != right; ++itr) mode_table_.erase(itr);
 }
 
 }  // namespace Just1RCe
--- srcs/in_memory_db_context/mapping_table.cc
+++ srcs/in_memory_db_context/mapping_table.cc
@@ -12,62 +12,70 @@ namespace Just1RCe {
  *
  * @brief add entry in both mapping table and mode table
  */
-bool InMemoryDbContext::JoinClientToChannel(std::string const &client_nick_name,
+bool InMemoryDbContext::JoinClientToChannel(int const client_fd,
                                             std::string const &channel_name) {
   // add entry in the mapping table
   client_to_channel_mapping_table_.insert(
-      std::make_pair(client_nick_name, channel_name));
+      std::make_pair(client_fd, channel_name));
   channel_to_client_mapping_table_.insert(
-      std::make_pair(channel_name, client_nick_name));
-  mode_table_.insert(std::make_pair(client_nick_name, channel_name));
+      std::make_pair(channel_name, client_fd));
+
+  ModeKey key = std::make_pair(client_fd, channel_name);
+  mode_table_.insert(std::make_pair(key, 0));
 }
 
 /**
  *
  * @brief delete entry in both mapping table and mode table
  */
-void InMemoryDbContext::PartClientFromChannel(
-    std::string const &client_nick_name, std::string const &channel_name) {
+void InMemoryDbContext::PartClientFromChannel(int const client_fd,
+                                              std::string const &channel_name) {
   // delete entry in client_to_channel_mapping_table_
-  // get half open range that has key value as 'client_nick_name'
-  MappingTableIter left =
-      client_to_channel_mapping_table_.lower_bound(client_nick_name);
-  MappingTableIter right =
-      client_to_channel_mapping_table_.upper_bound(client_nick_name);
-  // find erase target iterator
-  MappingTableIter erase_target_itr =
-      std::find(left, right, std::make_pair(client_nick_name, channel_name));
-  // if target iterator found, erase
-  if (erase_target_itr != right)
-    client_to_channel_mapping_table_.erase(erase_target_itr);
+  {
+    // get half open range that has key as 'client_fd'
+    ClientToChannelMappingTableIter left =
+        client_to_channel_mapping_table_.lower_bound(client_fd);
+    ClientToChannelMappingTableIter right =
+        client_to_channel_mapping_table_.upper_bound(client_fd);
+    // find target iter to erase
+    ClientToChannelMappingTableIter erase_target_itr =
+        std::find(left, right, std::make_pair(client_fd, channel_name));
+    // if target iterator found, erase
+    if (erase_target_itr != right)
+      client_to_channel_mapping_table_.erase(erase_target_itr);
+  }
 
   // delete entry in channel_to_client_mapping_table_
-  // get half open range that has key value as 'channel_name'
-  left = channel_to_client_mapping_table_.lower_bound(channel_name);
-  right = channel_to_client_mapping_table_.upper_bound(channel_name);
-  // find erase target iterator
-  erase_target_itr =
-      std::find(left, right, std::make_pair(channel_name, client_nick_name));
-  // if target iterator found, erase
-  if (erase_target_itr != right)
-    channel_to_client_mapping_table_.erase(erase_target_itr);
+  {
+    // get half open range that has key as 'channel_name'
+    ChannelToClientMappingTableIter left =
+        channel_to_client_mapping_table_.lower_bound(channel_name);
+    ChannelToClientMappingTableIter right =
+        channel_to_client_mapping_table_.upper_bound(channel_name);
+    // find target iterator to erase
+    ChannelToClientMappingTableIter erase_target_itr =
+        std::find(left, right, std::make_pair(channel_name, client_fd));
+    // if target iterator found, erase
+    if (erase_target_itr != right)
+      channel_to_client_mapping_table_.erase(erase_target_itr);
+  }
 
   // erase client mode
-  mode_table_.erase(std::make_pair(client_nick_name, channel_name));
+  DeleteClientMode(client_fd, channel_name);
 }
 
-std::vector<Channel *> InMemoryDbContext::GetChannelsByClientNickName(
-    std::string const &client_nick_name) {
+std::vector<Channel *> InMemoryDbContext::GetChannelsByClientFd(
+    int const client_fd) {
   std::vector<Channel *> channels;
 
   // get range
-  MappingTableIter left =
-      client_to_channel_mapping_table_.lower_bound(client_nick_name);
-  MappingTableIter right =
-      client_to_channel_mapping_table_.upper_bound(client_nick_name);
+  ClientToChannelMappingTableIter left =
+      client_to_channel_mapping_table_.lower_bound(client_fd);
+  ClientToChannelMappingTableIter right =
+      client_to_channel_mapping_table_.upper_bound(client_fd);
 
   // transform channel name too channel pointers
-  for (MappingTableIter itr = left; itr != right; ++itr) {
+  for (ClientToChannelMappingTableIter itr = left; itr != right; ++itr) {
     channels.push_back(channel_table_[itr->second]);
   }
   return channels;
@@ -78,13 +86,13 @@ std::vector<Client *> InMemoryDbContext::GetClientsByChannelName(
   std::vector<Client *> clients;
 
   // get range
-  MappingTableIter left =
+  ChannelToClientMappingTableIter left =
       channel_to_client_mapping_table_.lower_bound(channel_name);
-  MappingTableIter right =
+  ChannelToClientMappingTableIter right =
       channel_to_client_mapping_table_.upper_bound(channel_name);
 
   // transform channel name too channel pointers
-  for (MappingTableIter itr = left; itr != right; ++itr) {
+  for (ChannelToClientMappingTableIter itr = left; itr != right; ++itr) {
     clients.push_back(client_table_[itr->second]);
   }
   return clients;
