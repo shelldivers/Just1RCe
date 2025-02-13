60610fc8a260ae1ce0da470c1eebafa89fe6d17b
kenWR <skyqkqk1168@gmail.com>
2025-02-14 08:31:32 +0900

    chore: 잘못된 멤버 함수 표기 정정

--- includes/commands/nickCommandHandler.h
+++ includes/commands/nickCommandHandler.h
@@ -3,20 +3,22 @@
 
 #include <string.h>
 
-namespace Just1RCe {
+#include <vector>
 
-#include "../command_handler.h";
-class ContextHolder;
+#include "../command_handler.h"
+class DbContext;
+
+namespace Just1RCe {
 
 class NickCommandHandler : public Just1RCe::CommandHandler {
  private:
   NickCommandHandler(const NickCommandHandler& rhs);
   NickCommandHandler& operator=(const NickCommandHandler& rhs);
-  const int NickCommandHandler::checkNicknameForbidden(
-      const std::string& nickname);
-  const int NickCommandHandler::announceNicknameChanged(
-      DbContext* db, const Client* client, const std::string& old_nickname,
-      const std::string& new_nickname, std::vector<int>* fd_list);
+  const int checkNicknameForbidden(const std::string& nickname);
+  const int announceNicknameChanged(DbContext* db, const Client* client,
+                                    const std::string& old_nickname,
+                                    const std::string& new_nickname,
+                                    std::vector<int>* fd_list);
 
  public:
   NickCommandHandler();
