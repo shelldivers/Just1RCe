d5e883b72d047b4654a93e2d4b72a541b17920c8
Dohoon Gwak <96526280+eldeshue@users.noreply.github.com>
2025-02-18 20:49:57 +0900

    37 feat service routine (#96)
    * feat(Service) : declare RunIrcServer
    
    declare RunIrcServer and it's helper functions
    
    * feat(Service) : event handling helper functions
    
    이벤트를 관리하기 위해서 필요한 여러 helper 함수들
    주로 이벤트를 등록하고 변경하며 제거하는 함수들임
    r
    
    * feat(Service) : implement event handler
    
    이벤트가 발생한 대상이 server인지 client인지에 따라
    그 동작이 다르므로, 이를 별도로 구현해줘야 함.
    
    * feat(Service) : implement service loop
    
    실제 무한 루프를 돌면서 event를 대기 및 처리하는 루틴
    
    epoll로 구현한 관계로 Linux에서만 동작함.
    
    * chore : add brace
    
    * fix : minor fix

---
+++ srcs/service/run_irc_server.cc
@@ -0,0 +1,51 @@
+
+extern "C" {
+#include <sys/epoll.h>
+}
+
+#include <stdexcept>
+#include <string>
+
+#include "../../includes/command_handler.h"
+#include "../../includes/service.h"
+#include "../../includes/tcp_socket.h"
+
+namespace Just1RCe {
+
+/**
+ *
+ * @brief service routine of the irc server
+ * @warning because of using epoll, it works only on Linux
+ * @param port_number port number in string, to make server socket
+ * @param cmd_map map command string to command handler
+ *
+ */
+void Service::RunIrcServer(std::string const &port_number,
+                           CommandMapping const &cmd_map) {
+  // get kernel event queue, epoll
+  int const epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1) throw std::runtime_error("epoll creation failed");
+  struct epoll_event event_buffer[JUST1RCE_SRCS_EVENT_BUFFER_SIZE];
+
+  // get server socket(listening socket)
+  TcpSocket server(port_number);
+  RegisterServer(epoll_fd, server.socket_fd());
+
+  while (true) {
+    // get events from event queue
+    int const recv_event_cnt =
+        epoll_wait(epoll_fd, event_buffer, JUST1RCE_SRCS_EVENT_BUFFER_SIZE, -1);
+    if (recv_event_cnt == -1) throw std::runtime_error("epoll_wait failed");
+
+    // handling events
+    for (int i = 0; i < recv_event_cnt; ++i) {
+      if (event_buffer[i].data.fd == server.socket_fd()) {
+        HandleServerEvent(epoll_fd, cmd_map, event_buffer[i]);
+      } else {
+        HandleClientEvent(epoll_fd, cmd_map, event_buffer[i]);
+      }
+    }
+  }
+}
+
+}  // namespace Just1RCe
