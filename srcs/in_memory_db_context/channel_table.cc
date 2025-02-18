122c192c479a427fc76992685ee33d69f36d9802
Dohoon Gwak <96526280+eldeshue@users.noreply.github.com>
2025-02-18 20:10:02 +0900

    47 feat inmemorydbcontext (#81)
    * feat(InMemoryDbContext) : define InMemoryDbContext class
    
    DbContext 인터페이스를 구현한 InMemoryDbContext를 정의하여
    자원 관리를 일임한다.
    
    * feat(InMemoryDbContext) : implement construcot & destructor
    
    * feat : implement client related methods
    
    client객체 생성, 소멸, 획득 관련 메서드 추가
    fd로 해당 소켓의 클라이언트의 nick name을 획득하는 메서드 추가
    client의 소멸 시, client와 관련된 여러 mode도 함께 소멸
    
    * feat : add methods related to channel
    
    채널 관련 삽입, 삭제, 접근 메서드 추가
    
    * feat : add methods related to mode table
    
    * feat : implement methods related to mapping table between client and channel
    
    * feat : change definition based on DbContext
    
    DbContext 인터페이스의 변경에 따라 수정
    
    * feat : modify implementation based on DbContext
    
    DbContext의 변화(fd를 primary key로 변경)에 맞춰서 구현 변경
    
    * feat : modify code based on DbContext
    
    DbContext의 변화에 따라 구현을 변경
    - primary key를 client의 fd로 변경함에 따른 매개변수 변경
    - DelClient 수행 시 client에 의존하는 데이터 삭제 추가
    - GetNickNameByFd 메서드 삭제
    
    * fix : add return statement.
    
    * fix : fix invalidate iterator error
    
    매 순회를 통해서 조건에 만족하는 iterator를 찾은 다음에 erase함
    더 이상 조건을 만족하는 iterator를 찾지 못하면 while 탈출함
    
    iterator를 이용하여 순회하는 동시에 erase하여
    iterator를 invalidate하는 오류를 해결
    
    * fix : fix wrongly setted include path
    
    * chore : add comments

---
+++ srcs/in_memory_db_context/channel_table.cc
@@ -0,0 +1,44 @@
+
+#include <iterator>
+#include <string>
+#include <utility>
+
+#include "../../includes/in_memory_db_context.h"
+
+namespace Just1RCe {
+
+/**
+ * @warning check channel name's validity before creating channel
+ * @return if room is NULL, return false
+ */
+bool InMemoryDbContext::AddChannel(Channel *room) {
+  if (room == NULL) return false;
+
+  channel_table_.insert(std::make_pair(room->name(), room));
+
+  return true;
+}
+
+/**
+ * @brief delete channel from DbContext
+ * @warning before calling this, must part all client from the channel
+ */
+void InMemoryDbContext::DelChannel(std::string const &channel_name) {
+  // sanity check
+  if (!channel_table_.count(channel_name)) return;
+  // delete channel
+  delete channel_table_[channel_name];
+  channel_table_.erase(channel_name);
+}
+
+Channel *InMemoryDbContext::GetChannel(std::string const &channel_name) {
+  if (!channel_table_.count(channel_name)) return NULL;
+  return channel_table_[channel_name];
+}
+
+size_t InMemoryDbContext::GetNumOfClientInChannel(
+    std::string const &channel_name) {
+  return channel_to_client_mapping_table_.count(channel_name);
+}
+
+}  // namespace Just1RCe
---
+++ srcs/in_memory_db_context/client_table.cc
@@ -0,0 +1,65 @@
+
+#include <string>
+#include <utility>
+
+#include "../../includes/in_memory_db_context.h"
+
+namespace Just1RCe {
+
+/**
+ *
+ * @brief add client to the table
+ * @warning caller must check user's nick name is valid before creating client
+ * @return return false if the value of user is NULL
+ */
+bool InMemoryDbContext::AddClient(Client *user) {
+  // sanity check
+  if (user == NULL) return false;
+
+  // add index table
+  client_table_.insert(std::make_pair(user->GetFd(), user));
+
+  return true;
+}
+
+/**
+ *
+ * @brief delete client and it's related data
+ */
+void InMemoryDbContext::DelClient(int const client_fd) {
+  // sanity check
+  if (!client_table_.count(client_fd)) return;
+
+  // delete client's modes with 'client_fd'
+  DeleteClientModesByClientFd(client_fd);
+
+  // erase all entry with key as client_fd
+  client_to_channel_mapping_table_.erase(client_fd);
+
+  // erase all entry with value as client_fd
+  while (true) {
+    // erase one per loop
+    bool isFound = false;
+    for (ChannelToClientMappingTableIter itr =
+             channel_to_client_mapping_table_.begin();
+         itr != channel_to_client_mapping_table_.end(); ++itr) {
+      if (itr->second == client_fd) {
+        isFound = true;
+        channel_to_client_mapping_table_.erase(itr);
+        break;
+      }
+    }
+    if (!isFound) break;
+  }
+
+  // delete client
+  delete client_table_[client_fd];
+  client_table_.erase(client_fd);
+}
+
+Client *InMemoryDbContext::GetClient(int const client_fd) {
+  if (!client_table_.count(client_fd)) return NULL;
+  return client_table_[client_fd];
+}
+
+}  // namespace Just1RCe
