136a238e260ccad813602f22bddcfb7cb5548e52
kenWR <skyqkqk1168@gmail.com>
2025-02-20 01:53:05 +0900

    feat: template array initialization only once

--- includes/response_generator.h
+++ includes/response_generator.h
@@ -7,12 +7,14 @@
 class Client;
 class Channel;
 
+#define JUST1RCE_MAX_NUMERIC 1000
+
 namespace Just1RCe {
 
 struct ResponseArguments {
   std::string client_name;   // %c, 클라이언트 이름
-  std::string nicknames;     // %n, 닉네임 목록 (optional)
   std::string channel_name;  // %h, 채널 (optional)
+  std::string nicknames;     // %n, 닉네임 목록 (optional)
   std::string topic;         // %t, 채널 주제 ((optional))
   std::string command_name;  // %d, 명령어
   std::string modstring;     // %m, 모드 문자열 (optional)
@@ -24,7 +26,10 @@ struct ResponseArguments {
 
 class ResponseGenerator {
  private:
-  static const char* response_templates_[1000];
+  static const char* response_templates_[JUST1RCE_MAX_NUMERIC];
+
+  ResponseGenerator(const ResponseGenerator&);
+  ResponseGenerator& operator=(const ResponseGenerator&);
 
  public:
   ResponseGenerator();
--- srcs/responser/response_generator.cc
+++ srcs/responser/response_generator.cc
@@ -10,6 +10,38 @@
 
 namespace Just1RCe {
 
+const char* ResponseGenerator::response_templates_[] = {
+    ":%s 001 %c :Welcome to the SHELLDIVERS Network, %n",
+    ":%s 002 %c :Your host is %s, running version %v",
+    ":%s 324 %c %h %m",
+    ":%s 331 %c %h :No topic is set",
+    ":%s 332 %c %h :%t",
+    ":%s 341 %c %n %h",
+    ":%s 353 %c = %h :%n{ %n}",
+    ":%s 366 %c %h :End of /NAMES list",
+    ":%s 401 %c <nickname> :No such nick/channel",
+    ":%s 403 %c %h :No such channel",
+    ":%s 404 %c %h :Cannot send to channel",
+    ":%s 405 %c %h :You have joined too many channels",
+    ":%s 409 %c :No origin specified",
+    ":%s 411 %c :No recipient given (%d)",
+    ":%s 412 %c :No text to send",
+    ":%s 431 %c :No nickname given",
+    ":%s 432 %c %n :Erroneus nickname",
+    ":%s 433 %c %n :Nickname is already in use",
+    ":%s 441 %c %n %h :They aren't on that channel",
+    ":%s 442 %c %h :You're not on that channel",
+    ":%s 443 %c %n %h :is already on channel",
+    ":%s 451 %c :You have not registered",
+    ":%s 461 %c %d :Not enough parameters",
+    ":%s 462 %c :You may not reregister",
+    ":%s 471 %c %h :Cannot join channel (+l)",
+    ":%s 473 %c %h :Cannot join channel (+i)",
+    ":%s 475 %c %h :Cannot join channel (+k)",
+    ":%s 476 %h :Bad Channel Mask",
+    ":%s 482 %c %h :You're not channel operator",
+};
+
 /**
  * @brief Construct a new Response Arguments:: Response Arguments object
  *
@@ -42,52 +74,69 @@ ResponseArguments::ResponseArguments(const Client& client,
 }
 
 ResponseGenerator::ResponseGenerator() {
-  response_templates_[RPL_WELCOME] =
-      ":%s 001 %c :Welcome to the SHELLDIVERS Network, %n";
-  response_templates_[RPL_YOURHOST] =
-      ":%s 002 %c :Your host is %s, running version %v";
-  response_templates_[RPL_CHANNELMODEIS] = ":%s 324 %c %h %m";
-  response_templates_[RPL_NOTOPIC] = ":%s 331 %c %h :No topic is set";
-  response_templates_[RPL_TOPIC] = ":%s 332 %c %h :%t";
-  response_templates_[RPL_INVITING] = ":%s 341 %c %n %h";
-  response_templates_[RPL_NAMREPLY] = ":%s 353 %c = %h :%n{ %n}";
-  response_templates_[RPL_ENDOFNAMES] = ":%s 366 %c %h :End of /NAMES list";
-  response_templates_[ERR_NOSUCHNICK] =
-      ":%s 401 %c <nickname> :No such nick/channel";
-  response_templates_[ERR_NOSUCHCHANNEL] = ":%s 403 %c %h :No such channel";
-  response_templates_[ERR_CANNOTSENDTOCHAN] =
-      ":%s 404 %c %h :Cannot send to channel";
-  response_templates_[ERR_TOOMANYCHANNELS] =
-      ":%s 405 %c %h :You have joined too many channels";
-  response_templates_[ERR_NOORIGIN] = ":%s 409 %c :No origin specified";
-  response_templates_[ERR_NORECIPIENT] = ":%s 411 %c :No recipient given (%d)";
-  response_templates_[ERR_NOTEXTTOSEND] = ":%s 412 %c :No text to send";
-  response_templates_[ERR_NONICKNAMEGIVEN] = ":%s 431 %c :No nickname given";
-  response_templates_[ERR_ERRONEUSNICKNAME] =
-      ":%s 432 %c %n :Erroneus nickname";
-  response_templates_[ERR_NICKNAMEINUSE] =
-      ":%s 433 %c %n :Nickname is already in use";
-  response_templates_[ERR_USERNOTINCHANNEL] =
-      ":%s 441 %c %n %h :They aren't on that channel";
-  response_templates_[ERR_NOTONCHANNEL] =
-      ":%s 442 %c %h :You're not on that channel";
-  response_templates_[ERR_USERONCHANNEL] =
-      ":%s 443 %c %n %h :is already on channel";
-  response_templates_[ERR_NOTREGISTERED] =
-      ":%s 451 %c :You have not registered";
-  response_templates_[ERR_NEEDMOREPARAMS] =
-      ":%s 461 %c %d :Not enough parameters";
-  response_templates_[ERR_ALREADYREGISTERED] =
-      ":%s 462 %c :You may not reregister";
-  response_templates_[ERR_CHANNELISFULL] =
-      ":%s 471 %c %h :Cannot join channel (+l)";
-  response_templates_[ERR_INVITEONLYCHAN] =
-      ":%s 473 %c %h :Cannot join channel (+i)";
-  response_templates_[ERR_BADCHANNELKEY] =
-      ":%s 475 %c %h :Cannot join channel (+k)";
-  response_templates_[ERR_BADCHANMASK] = ":%s 476 %h :Bad Channel Mask";
-  response_templates_[ERR_CHANOPRIVSNEEDED] =
-      ":%s 482 %c %h :You're not channel operator";
+  static bool initialized = false;
+
+  if (initialized == false) {
+    response_templates_[RPL_WELCOME] =
+        ":%s 001 %c :Welcome to the SHELLDIVERS Network, %c";
+    response_templates_[RPL_YOURHOST] =
+        ":%s 002 %c :Your host is %s, running version %v";
+    response_templates_[RPL_CHANNELMODEIS] = 
+	    ":%s 324 %c %h %m";
+    response_templates_[RPL_NOTOPIC] = 
+	    ":%s 331 %c %h :No topic is set";
+    response_templates_[RPL_TOPIC] = 
+	    ":%s 332 %c %h :%t";
+    response_templates_[RPL_INVITING] = 
+	    ":%s 341 %c %n %h"; // Need to check
+    response_templates_[RPL_NAMREPLY] = 
+	    ":%s 353 %c = %h :%n{ %n}"; // Need to check
+    response_templates_[RPL_ENDOFNAMES] = 
+	    ":%s 366 %c %h :End of /NAMES list";
+    response_templates_[ERR_NOSUCHNICK] =
+        ":%s 401 %c <nickname> :No such nick/channel";
+    response_templates_[ERR_NOSUCHCHANNEL] = ":%s 403 %c %h :No such channel";
+    response_templates_[ERR_CANNOTSENDTOCHAN] =
+        ":%s 404 %c %h :Cannot send to channel";
+    response_templates_[ERR_TOOMANYCHANNELS] =
+        ":%s 405 %c %h :You have joined too many channels";
+    response_templates_[ERR_NOORIGIN] = 
+	    ":%s 409 %c :No origin specified";
+    response_templates_[ERR_NORECIPIENT] =
+        ":%s 411 %c :No recipient given (%d)";
+    response_templates_[ERR_NOTEXTTOSEND] = 
+	    ":%s 412 %c :No text to send";
+    response_templates_[ERR_NONICKNAMEGIVEN] = 
+	    ":%s 431 %c :No nickname given";
+    response_templates_[ERR_ERRONEUSNICKNAME] =
+        ":%s 432 %c %n :Erroneus nickname"; // Need to check
+    response_templates_[ERR_NICKNAMEINUSE] =
+        ":%s 433 %c %n :Nickname is already in use"; // Need to check
+    response_templates_[ERR_USERNOTINCHANNEL] =
+        ":%s 441 %c %n %h :They aren't on that channel"; // Need to check
+    response_templates_[ERR_NOTONCHANNEL] =
+        ":%s 442 %c %h :You're not on that channel";
+    response_templates_[ERR_USERONCHANNEL] =
+        ":%s 443 %c %n %h :is already on channel"; // Need to check
+    response_templates_[ERR_NOTREGISTERED] =
+        ":%s 451 %c :You have not registered";
+    response_templates_[ERR_NEEDMOREPARAMS] =
+        ":%s 461 %c %d :Not enough parameters";
+    response_templates_[ERR_ALREADYREGISTERED] =
+        ":%s 462 %c :You may not reregister";
+    response_templates_[ERR_CHANNELISFULL] =
+        ":%s 471 %c %h :Cannot join channel (+l)";
+    response_templates_[ERR_INVITEONLYCHAN] =
+        ":%s 473 %c %h :Cannot join channel (+i)";
+    response_templates_[ERR_BADCHANNELKEY] =
+        ":%s 475 %c %h :Cannot join channel (+k)";
+    response_templates_[ERR_BADCHANMASK] = 
+	    ":%s 476 %h :Bad Channel Mask";
+    response_templates_[ERR_CHANOPRIVSNEEDED] =
+        ":%s 482 %c %h :You're not channel operator";
+
+    initialized = true;
+  }
 }
 
 ResponseGenerator::~ResponseGenerator() {}
