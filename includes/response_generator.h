278429441828071e2ce655433c92f01d7500df71
kenWR <skyqkqk1168@gmail.com>
2025-02-20 11:56:51 +0900

    feat: 최종본 완성
    - 싱글톤 으로 객체 유지 및 중복 초기화 방지

--- includes/response_generator.h
+++ includes/response_generator.h
@@ -12,30 +12,33 @@ class Channel;
 namespace Just1RCe {
 
 struct ResponseArguments {
-  std::string client_name;   // %c, 클라이언트 이름
-  std::string channel_name;  // %h, 채널 (optional)
-  std::string nicknames;     // %n, 닉네임 목록 (optional)
-  std::string topic;         // %t, 채널 주제 ((optional))
-  std::string command_name;  // %d, 명령어
-  std::string modstring;     // %m, 모드 문자열 (optional)
+  std::string client_name;          // %c, 클라이언트 이름
+  std::string channel_name;         // %h, 채널 (optional)
+  std::string nicknames;            // %n, 닉네임 (optional)
+  std::string topic;                // %t, 채널 주제 (optional)
+  std::string modstring;            // %m, 모드 문자열 (optional)
+  std::vector<std::string> params;  // %p, 파라미터 목록
 
-  ResponseArguments(const Client& client, const Channel& channel,
-                    const std::vector<Client*>& client_list,
-                    const std::string& command);
+  ResponseArguments(const int numeric, const Client& client,
+                    const Channel* channel, std::vector<std::string> params);
 };
 
 class ResponseGenerator {
  private:
-  static const char* response_templates_[JUST1RCE_MAX_NUMERIC];
+  static ResponseGenerator* instance_;
+  const char* response_templates_[JUST1RCE_MAX_NUMERIC];
 
+  ResponseGenerator();
   ResponseGenerator(const ResponseGenerator&);
   ResponseGenerator& operator=(const ResponseGenerator&);
+  void InitializeTemplates();
 
  public:
-  ResponseGenerator();
+  static ResponseGenerator* GetInstance();
+  static void DestroyInstance();
   ~ResponseGenerator();
-  std::string generate_response(const int numeric,
-                                const struct ResponseArguments& res_args);
+  std::string GenerateResponse(const int numeric,
+                               const struct ResponseArguments& res_args);
 };
 
 }  // namespace Just1RCe
