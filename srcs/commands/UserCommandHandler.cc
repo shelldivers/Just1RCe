e37e654db269173ac5a6660ba143d736cd530f05
정지원 <111475563+KenWR@users.noreply.github.com>
2025-02-23 17:32:00 +0900

    #85 feat user 명령어 구현 (#104)
    * feat: user command handler 구현 완료료
    
    * chore: 클래스 스코프 연산자 추가
    
    * style: change style of header guard
    
    * chore: 헤더 스타일 변경
    
    * refactor: refactoring user command handler
    
    * chore: 주석 추가가
    
    * 🚧 feat: work in progress...
    
    * feat: USER command handler is done
    
    * chore: fix lint
    
    * chore: memberfuction renamed
    
    * chore: fix lint

---
+++ srcs/commands/UserCommandHandler.cc
@@ -0,0 +1,82 @@
+#include "userCommandHandler.h"
+
+#include <string>
+#include <vector>
+
+#include "../../includes/client.h"
+#include "../../includes/command_handler.h"
+#include "../../includes/config.h"
+#include "../../includes/context_holder.h"
+#include "../../includes/dbcontext.h"
+#include "../../includes/numeric.h"
+#include "../../includes/parser.h"
+#include "../../includes/response_generator.h"
+
+namespace Just1RCe {
+
+UserCommandHandler::UserCommandHandler() {}
+
+UserCommandHandler::~UserCommandHandler() {}
+
+/**
+ * @brief USER command handler
+ *
+ * @param client_fd fd to identify client that the message originate from
+ * @param message raw text message from client
+ *
+ * @return vector of int(fd), to register write event
+ *
+ * @details
+ * Get 2 types of Numeric
+ * - ERR_NEEDMOREPARAMS : not enough parameters
+ * - ERR_ALREADYREGISTRED : already registered
+ */
+std::vector<int> UserCommandHandler::operator()(const int client_fd,
+                                                const std::string& message) {
+  Client* client = ContextHolder::GetInstance()->db()->GetClient(client_fd);
+
+  if (client == NULL || client->IsPassed() == false) {
+    return std::vector<int>();
+  }
+
+  // Parse
+  std::string username;
+  std::string realname;
+  Parser parser(message);
+  parser.ParseCommandUser(&username, &realname);
+
+  // Get numeric
+  int numeric = CheckUser(*client, username, realname);
+  if (numeric != IRC_NOERROR) {
+    ResponseGenerator& generator = ResponseGenerator::GetInstance();
+    std::string response = generator.GenerateResponse(
+        numeric,
+        ResponseArguments{numeric, *client, NULL, parser.GetTokenStream()});
+
+    client->SetSendMessage(response);
+    return std::vector<int>(1, client_fd);
+  }
+
+  // Set user name and real name
+  client->set_user_name(username);
+  client->set_real_name(realname);
+
+  return std::vector<int>();
+}
+
+const int UserCommandHandler::CheckUser(const Client& client,
+                                        std::string username,
+                                        std::string realname) {
+  if (username.empty() == true || realname.empty() == true) {
+    return ERR_NEEDMOREPARAMS;
+  }
+
+  if (client.user_name().empty() == false ||
+      client.real_name().empty() == false) {
+    return ERR_ALREADYREGISTERED;
+  }
+
+  return IRC_NOERROR;
+}
+
+}  // namespace Just1RCe
