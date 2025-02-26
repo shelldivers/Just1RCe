ef5ecac16544731b716aefd0822338ab5784bc1c
iphuck22 <eldeshue@naver.com>
2025-02-26 18:07:20 +0900

    feat : add all handlers to the command mapping

--- srcs/command_mapping/command_mapping.cc
+++ srcs/command_mapping/command_mapping.cc
@@ -4,11 +4,17 @@
 #include <string>
 
 #include "../../includes/command_handler.h"
+#include "../../includes/commands/invite_command_handler.h"
 #include "../../includes/commands/join_command_handler.h"
+#include "../../includes/commands/kick_command_handler.h"
+#include "../../includes/commands/mode_command_handler.h"
 #include "../../includes/commands/nick_command_handler.h"
+#include "../../includes/commands/part_command_handler.h"
 #include "../../includes/commands/pass_command_handler.h"
 #include "../../includes/commands/ping_command_handler.h"
+#include "../../includes/commands/privmsg_command_handler.h"
 #include "../../includes/commands/quit_command_handler.h"
+#include "../../includes/commands/topic_command_handler.h"
 #include "../../includes/commands/user_command_handler.h"
 
 namespace Just1RCe {
@@ -21,6 +27,12 @@ void RegistCommandHandlers(CommandMapping* cmd_map,
   (*cmd_map)["PING"] = new PingCommandHandler();
   (*cmd_map)["JOIN"] = new JoinCommandHandler();
   (*cmd_map)["QUIT"] = new QuitCommandHandler();
+  (*cmd_map)["PART"] = new PartCommandHandler();
+  (*cmd_map)["INVITE"] = new InviteCommandHandler();
+  (*cmd_map)["TOPIC"] = new TopicCommandHandler();
+  (*cmd_map)["KICK"] = new KickCommandHandler();
+  (*cmd_map)["MODE"] = new ModeCommandHandler();
+  (*cmd_map)["PRIVMSG"] = new PrivmsgCommandHandler();
 }
 
 }  // namespace Just1RCe
