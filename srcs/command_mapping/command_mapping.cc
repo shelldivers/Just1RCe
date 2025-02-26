a559befe56b25b89da55dc1ba4d4966922584706
정지원 <111475563+KenWR@users.noreply.github.com>
2025-02-26 16:39:11 +0900

    #88 feat join 명령어 구현 (#127)
    * feat: JOIN commnad handler 구현 완료료
    
    * feat: 표기 실수 수정 및 헤더파일에 멤버 함수 추가가
    
    * chore: 주석 추가 및 존재하지 않는 메소드 제거 및 주석처리리
    
    * 🚧 chore: rename files.
    
    * chore: store works before pull from dev.
    
    * feat: JOIN command handler 구현 완료
    
    * feat: badchannelmask added
    
    * chore: clean up useless annotation
    
    * feat: badchannelmask part from CheckChannel function.
    
    now CheckChannel function is CheckChannelMode.
    
    CheckChannelName function added.
    
    * chore: AnnounceJoined -> BroadcastJoined function renamed.
    
    * feat: ERR_NEEDMOREPARAMS case added
    
    * feat: mapping command handlers.
    
    * feat: 구현 및 리팩토링 완료

--- srcs/command_mapping/command_mapping.cc
+++ srcs/command_mapping/command_mapping.cc
@@ -4,8 +4,11 @@
 #include <string>
 
 #include "../../includes/command_handler.h"
+#include "../../includes/commands/join_command_handler.h"
 #include "../../includes/commands/nick_command_handler.h"
 #include "../../includes/commands/pass_command_handler.h"
+#include "../../includes/commands/ping_command_handler.h"
+#include "../../includes/commands/quit_command_handler.h"
 #include "../../includes/commands/user_command_handler.h"
 
 namespace Just1RCe {
@@ -15,6 +18,9 @@ void RegistCommandHandlers(CommandMapping* cmd_map,
   (*cmd_map)["PASS"] = new PassCommandHandler(password);
   (*cmd_map)["NICK"] = new NickCommandHandler();
   (*cmd_map)["USER"] = new UserCommandHandler();
+  (*cmd_map)["PING"] = new PingCommandHandler();
+  (*cmd_map)["JOIN"] = new JoinCommandHandler();
+  (*cmd_map)["QUIT"] = new QuitCommandHandler();
 }
 
 }  // namespace Just1RCe
