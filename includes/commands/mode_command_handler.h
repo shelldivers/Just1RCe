8647c077fe9a1e7e1f26b1a18c218a551ab0d393
Dohoon Gwak <96526280+eldeshue@users.noreply.github.com>
2025-02-26 17:58:52 +0900

    #125 feat mode 명령어 구현 (#142)
    * feat : implement mode command handler
    
    client 및 channel의 권한을 표현하는 mode를 수정하는 command
    MODE 커맨드를 처리하는 루틴을 구현한다.
    
    * feat : implement invite only mode
    
    해당 채널을 초대 전용 채널로 설정하는 i모드를 설정 및 제거한다.
    
    * feat : implement key mode
    
    해당 채널의 입장과 관련하여 비밀번호를 설정하는 k 모드를 구현한다
    
    * feat : implement limit mode
    
    해당 채널의 인원수에 제한을 설정하는 limit mode를 구현한다.
    
    * feat : implement operator mode
    
    해당 채널에 관리자를 설정하는 operator mode를 구현한다.
    
    * feat : implement topic mode
    
    해당 채널에 topic을 관리자만 설정할 수 있도록 제한하는 모드
    
    * fix : erase scope specifier.
    
    * fix : erase useless bracket.
    
    * feat : add client authentication routine

---
+++ includes/commands/mode_command_handler.h
@@ -0,0 +1,49 @@
+
+#ifndef JUST1RCE_INCLUDES_COMMANDS_MODE_COMMAND_HANDLER_H
+#define JUST1RCE_INCLUDES_COMMANDS_MODE_COMMAND_HANDLER_H
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../command_handler.h"
+
+namespace Just1RCe {
+
+class ModeCommandHandler : public CommandHandler {
+ private:
+  ModeCommandHandler(ModeCommandHandler const &);
+  ModeCommandHandler &operator=(ModeCommandHandler const &);
+
+  // common utility
+  void BroadCastMode(std::vector<int> *fd_list, Channel *const target_channel,
+                     Client *const target_client, char const mode_oprt,
+                     char const mode_identifier, std::string const &mode_arg);
+
+  void ModifyModes(Client *const target_client, Channel *const target_channel,
+                   std::string const &mode_string, std::string const &mode_args,
+                   std::vector<int> *fd_list);
+  // mode to handle
+  void ProcessModeI(Client *const target_client, Channel *const target_channel,
+                    std::vector<int> *fd_list, char const oprt);
+  void ProcessModeT(Client *const target_client, Channel *const target_channel,
+                    std::vector<int> *fd_list, char const oprt);
+  void ProcessModeK(Client *const target_client, Channel *const target_channel,
+                    std::vector<int> *fd_list, char const oprt,
+                    std::stringstream *arguments);
+  void ProcessModeO(Client *const target_client, Channel *const target_channel,
+                    std::vector<int> *fd_list, char const oprt,
+                    std::stringstream *arguments);
+  void ProcessModeL(Client *const target_client, Channel *const target_channel,
+                    std::vector<int> *fd_list, char const oprt,
+                    std::stringstream *arguments);
+
+ public:
+  ModeCommandHandler();
+  ~ModeCommandHandler();
+  std::vector<int> operator()(const int client_fd, const std::string &message);
+};
+
+}  // namespace Just1RCe
+
+#endif
---
+++ srcs/commands/mode_command_handler.cc
@@ -0,0 +1,147 @@
+
+#include "../../includes/commands/mode_command_handler.h"
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../../includes/channel.h"
+#include "../../includes/client.h"
+#include "../../includes/command_handler.h"
+#include "../../includes/context_holder.h"
+#include "../../includes/dbcontext.h"
+#include "../../includes/numeric.h"
+#include "../../includes/parser.h"
+#include "../../includes/response_generator.h"
+
+namespace Just1RCe {
+
+ModeCommandHandler::ModeCommandHandler() {}
+ModeCommandHandler::~ModeCommandHandler() {}
+
+void ModeCommandHandler::BroadCastMode(std::vector<int> *fd_list,
+                                       Channel *const target_channel,
+                                       Client *const target_client,
+                                       char const mode_oprt,
+                                       char const mode_identifier,
+                                       std::string const &mode_arg) {
+  std::vector<Client *> clients =
+      ContextHolder::GetInstance()->db()->GetClientsByChannelName(
+          target_channel->name());
+
+  std::stringstream message;
+  message << ':' << target_client->nick_name() << '!'
+          << target_client->user_name() << "@" << target_client->GetHostName()
+          << ".IP MODE " << target_channel->name() << ' ' << mode_oprt
+          << mode_identifier << ' ' << mode_arg;
+
+  for (size_t i = 0; i < clients.size(); ++i) {
+    clients[i]->SetSendMessage(message.str());
+    fd_list->push_back(clients[i]->GetFd());
+  }
+}
+
+static bool CheckModeStringFormat(std::string const &mode_string);
+
+std::vector<int> ModeCommandHandler::operator()(const int client_fd,
+                                                const std::string &message) {
+  // parse message and get resources
+  std::string target_channel_name, mode_string, mode_arguments;
+  Parser parser(message);
+  parser.ParseCommandMode(&target_channel_name, &mode_string, &mode_arguments);
+
+  Client *const target_client =
+      ContextHolder::GetInstance()->db()->GetClient(client_fd);
+  Channel *const target_channel =
+      ContextHolder::GetInstance()->db()->GetChannel(target_channel_name);
+
+  // client auth check
+  if (!target_client->IsAuthenticated()) return std::vector<int>();
+
+  // target channel not found
+  if (target_channel == NULL) {
+    std::string response = ResponseGenerator::GetInstance().GenerateResponse(
+        ERR_NOSUCHCHANNEL,
+        ResponseArguments(ERR_NOSUCHCHANNEL, *target_client, target_channel,
+                          parser.GetTokenStream()));
+    target_client->SetSendMessage(response);
+    return std::vector<int>(1, client_fd);
+  }
+
+  // check input parameter
+  if (CheckModeStringFormat(mode_string)) {
+    std::string response = ResponseGenerator::GetInstance().GenerateResponse(
+        RPL_CHANNELMODEIS,
+        ResponseArguments(RPL_CHANNELMODEIS, *target_client, target_channel,
+                          parser.GetTokenStream()));
+    target_client->SetSendMessage(response);
+    return std::vector<int>(1, client_fd);
+  }
+
+  // check client authority
+  if (target_channel->GetOperatorFd() != client_fd) {
+    std::string response = ResponseGenerator::GetInstance().GenerateResponse(
+        ERR_CHANOPRIVSNEEDED,
+        ResponseArguments(ERR_CHANOPRIVSNEEDED, *target_client, target_channel,
+                          parser.GetTokenStream()));
+    target_client->SetSendMessage(response);
+    return std::vector<int>(1, client_fd);
+  }
+
+  // do MODE command
+  std::vector<int> fd_list;
+  ModifyModes(target_client, target_channel, mode_string, mode_arguments,
+              &fd_list);
+  return fd_list;
+}
+
+static bool CheckModeIdentifier(char const c) {
+  return ((c == 'i') || (c == 't') || (c == 'k') || (c == 'o') || (c == 'l'));
+}
+
+static bool CheckModeOperator(char const c) {
+  return ((c == '+') || (c == '-'));
+}
+
+static bool CheckModeStringFormat(std::string const &mode_string) {
+  if (mode_string.empty() || !CheckModeOperator(mode_string[0])) {
+    return false;
+  }
+
+  for (size_t i = 1; i < mode_string.size(); ++i) {
+    if (!CheckModeIdentifier(mode_string[i])) return false;
+  }
+  return true;
+}
+
+void ModeCommandHandler::ModifyModes(Client *const target_client,
+                                     Channel *const target_channel,
+                                     std::string const &mode_string,
+                                     std::string const &mode_args,
+                                     std::vector<int> *fd_list) {
+  std::stringstream ss(mode_args);
+  char oprt = '\0';
+
+  for (int i = 0; i < mode_string.size(); ++i) {
+    if (CheckModeOperator(mode_string[i])) {
+      // change method
+      oprt = mode_string[i];
+    } else {
+      if (mode_string[i] == 'i') {
+        ProcessModeI(target_client, target_channel, fd_list, oprt);
+      } else if (mode_string[i] == 't') {
+        ProcessModeT(target_client, target_channel, fd_list, oprt);
+      } else if (mode_string[i] == 'k') {
+        ProcessModeK(target_client, target_channel, fd_list, oprt, &ss);
+      } else if (mode_string[i] == 'o') {
+        ProcessModeO(target_client, target_channel, fd_list, oprt, &ss);
+      } else if (mode_string[i] == 'l') {
+        ProcessModeL(target_client, target_channel, fd_list, oprt, &ss);
+      } else {
+        // unreachable, unknown mode, skip
+      }
+    }
+  }
+}
+
+}  // namespace Just1RCe
