2c849ffabfdc0063ccdf74ec65278ae95ecd6f54
iphuck22 <eldeshue@naver.com>
2025-02-28 00:50:27 +0900

    fix: add skip user's mode modification

--- srcs/commands/mode_command_handler.cc
+++ srcs/commands/mode_command_handler.cc
@@ -43,6 +43,8 @@ void ModeCommandHandler::BroadCastMode(std::vector<int> *fd_list,
 
 static bool CheckModeStringFormat(std::string const &mode_string);
 
+static bool CheckChannelNameFormat(std::string const &channel_name);
+
 std::vector<int> ModeCommandHandler::operator()(const int client_fd,
                                                 const std::string &message) {
   // parse message and get resources
@@ -58,14 +60,9 @@ std::vector<int> ModeCommandHandler::operator()(const int client_fd,
   // client auth check
   if (!target_client->IsAuthenticated()) return std::vector<int>();
 
-  // target channel not found
-  if (target_channel == NULL) {
-    std::string response = ResponseGenerator::GetInstance().GenerateResponse(
-        ERR_NOSUCHCHANNEL,
-        ResponseArguments(ERR_NOSUCHCHANNEL, *target_client, target_channel,
-                          parser.GetTokenStream()));
-    target_client->SetSendMessage(response);
-    return std::vector<int>(1, client_fd);
+  // no user mode, no reply
+  if (!CheckChannelNameFormat(target_channel_name)) {
+    return std::vector<int>();
   }
 
   // check input parameter
@@ -78,6 +75,15 @@ std::vector<int> ModeCommandHandler::operator()(const int client_fd,
     return std::vector<int>(1, client_fd);
   }
 
+  // target channel not found
+  if (target_channel == NULL) {
+    std::string response = ResponseGenerator::GetInstance().GenerateResponse(
+        ERR_NOSUCHCHANNEL,
+        ResponseArguments(ERR_NOSUCHCHANNEL, *target_client, target_channel,
+                          parser.GetTokenStream()));
+    target_client->SetSendMessage(response);
+    return std::vector<int>(1, client_fd);
+  }
   // check client authority
   if (target_channel->GetOperatorFd() != client_fd) {
     std::string response = ResponseGenerator::GetInstance().GenerateResponse(
@@ -112,6 +118,11 @@ static bool CheckModeStringFormat(std::string const &mode_string) {
     if (!CheckModeIdentifier(mode_string[i])) return false;
   }
   return true;
+}
+
+static bool CheckChannelNameFormat(std::string const &channel_name) {
+  return ((!channel_name.empty()) &&
+          ((channel_name[0] == '#') || (channel_name[0] == '&')));
 }
 
 void ModeCommandHandler::ModifyModes(Client *const target_client,
