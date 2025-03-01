7a4275061d01a992b50911384c57a53b2600c013
Dohoon Gwak <96526280+eldeshue@users.noreply.github.com>
2025-03-01 05:25:52 +0000

    chore: erase unnecessary files

--- tests/test_main.cc
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-
-// Google Test 실행을 위한 메인 함수
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);  // Google Test 초기화
-    return RUN_ALL_TESTS();                 // 모든 테스트 실행
-}
